0f05f85483a38ea0a45e78609c2ff9b2275ac7c3
Mehmet Sezer <mehmetszr05@gmail.com>
2024-02-05 19:02:07 +0300

    Footer and back to top button has been added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
+//#region splash
 setTimeout(() => {
     document.getElementById('Splash').classList.toggle('fade');
 }, 2000);
+//#endregion splash
 
+
+//#region qna
 qnaButtons = document.getElementsByClassName('qna-btn');
 
 for(let i = 0, len = qnaButtons.length; i < len; i++) {
@@ -9,3 +13,26 @@ for(let i = 0, len = qnaButtons.length; i < len; i++) {
         qnaButtons[i].parentNode.parentNode.classList.toggle('active');
     })
 }
+//#endregion qna
+
+//#region back-to-top
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 200) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+backTopBtn.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+})
+
+window.addEventListener("scroll", activeElementOnScroll);
+
+//#endregion back-to-top
